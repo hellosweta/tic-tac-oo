b2b9d2ae05efaee446df8f020270c0024babf187
Sweta Sanghavi <swetagsanghavi@gmail.com>
2020-03-24 10:32:24 -0400

    pass in standard input

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -2,8 +2,19 @@ require_relative "game.rb"
 
 class GameRunner
   def run
-    output = Game.run(StringIO.new)
+    $stdin = StringIO.new("input")
+    output = Game.run($stdin)
+
+    restore_standard_input
+
+    output.rewind
     puts output.string
+  end
+
+  private
+
+  def restore_standard_input
+    $stdin = STDIN
   end
 end
 
