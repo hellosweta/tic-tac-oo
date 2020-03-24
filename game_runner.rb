2891b589e3fd2680eb7282fe9cc5d2e5218d69da
Sweta <swetagsanghavi@gmail.com>
2020-03-24 15:54:26 -0400

    Update gamerunner to accept standard output (#6)
    Update GameRunner to accept standard output

--- game_runner.rb
+++ game_runner.rb
@@ -1,23 +1,3 @@
-class GameRunner
-  def self.run(*args)
-    GameRunner.new.run(*args)
-  end
+require_relative "lib/game.rb"
 
-  def run(output)
-    output.puts empty_board
-  end
-
-  def empty_board
-    <<~SQUIGGLY_HEREDOC
-       __ __ __
-      |  |  |  |
-      |__|__|__|
-      |  |  |  |
-      |__|__|__|
-      |  |  |  |
-      |__|__|__|
-    SQUIGGLY_HEREDOC
-  end
-end
-
-GameRunner.run(StringIO.new)
+Game.run($stdout)
