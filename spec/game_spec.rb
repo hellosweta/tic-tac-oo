9a18ceb057f7f1cb6227a54fa767793bc9d05b39
Sweta Sanghavi <swetagsanghavi@gmail.com>
2020-03-30 11:07:13 -0400

    Failing acceptance test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/game"
 
 RSpec.describe Game do
   describe "#run" do
-    it "plays tic tac toe" do
+    it "returns an error when user enters an invalid coordinate" do
       start_message = <<~SQUIGGLY_HEREDOC
            1  2  3
            __ __ __
@@ -20,24 +20,15 @@ RSpec.describe Game do
 
       mock_stdout = StringIO.new
       mock_stdin = StringIO.new
-      allow(mock_stdin).to receive(:gets).and_return("1A")
+      allow(mock_stdin).to receive(:gets).and_return("1A", "A1")
 
       Game.run(mock_stdout, mock_stdin)
 
       expect(mock_stdout.string).to include(start_message)
       expect(mock_stdout.string).to include("Error")
-
-      # The app waits for user input after rendering the screen with
-      # a prompt asking for coordinates. When the user enters a good
-      # coordinate the app quits. When the user enters bad coordinates,
-      # the application returns a message about not understanding
-      # the message, then waits for good input. The application doesn’t
-      # quit until the user enters good information. Coordinates are
-      # a row letter combined with a column number,
-      # for example: A2, B3.
     end
 
-    it "exits when user inputs a valid response" do
+    it "exits when user inputs a valid coordinate" do
       mock_stdout = StringIO.new
       mock_stdin = StringIO.new
       allow(mock_stdin).to receive(:gets).and_return("A1")
