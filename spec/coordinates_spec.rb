4035b4a8b4432bfb9dd86248e85b06289a131d18
Sweta Sanghavi <swetagsanghavi@gmail.com>
2020-03-31 17:59:53 -0400

    Have specific edge cases to test

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/coordinates"
 
 RSpec.describe Coordinates do
   describe "valid?" do
-    it "returns false when row is not valid" do
+    it "returns false when user entered row is below last row" do
       input = "d1"
 
       coordinates = Coordinates.new(input)
@@ -11,7 +11,15 @@ RSpec.describe Coordinates do
       expect(coordinates.valid?).to be false
     end
 
-    it "returns false when column is not valid" do
+    it "returns false when user entered column is above first column" do
+      input = "a0"
+
+      coordinates = Coordinates.new(input)
+
+      expect(coordinates.valid?).to be false
+    end
+
+    it "returns false when entered column is past last column" do
       input = "a4"
 
       coordinates = Coordinates.new(input)
@@ -19,7 +27,23 @@ RSpec.describe Coordinates do
       expect(coordinates.valid?).to be false
     end
 
-    it "returns true when row and column are valid" do
+    it "returns false when length of user input is greater than 2" do
+      input = "a43"
+
+      coordinates = Coordinates.new(input)
+
+      expect(coordinates.valid?).to be false
+    end
+
+    it "returns false when length of user input is less than 2" do
+      input = "a"
+
+      coordinates = Coordinates.new(input)
+
+      expect(coordinates.valid?).to be false
+    end
+
+    it "returns true when row and column are within bounds of column and rows" do
       input = "A3"
 
       coordinates = Coordinates.new(input)
